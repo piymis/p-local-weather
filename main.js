1bc6958ead59b173a13cc8b10aefbdee59f53896
Piyush Mishra <piyush11mishra@gmail.com>
2017-06-28 21:45:38 +0530

    Added weather icon and units

---
+++ main.js
@@ -0,0 +1,73 @@
+const weatherMapApiUrl = 'https://api.apixu.com/v1/current.json?key=ab4c157f6f6644d182e205155172706&q={lat},{lon}';
+const userLocationApiUrl = 'https://freegeoip.net/json/';
+var isCelsius = true;
+var weatherData;
+var userLocation;
+
+// Update weather on page load
+document.addEventListener("DOMContentLoaded", updateWeatherOnLoad, false);
+document.getElementById('weather-unit').addEventListener('click', changeWeatherUnit, false);
+
+
+function updateWeatherOnLoad() {
+    getLocationAndWeather();
+}
+
+
+function changeWeatherUnit() {
+    isCelsius = !isCelsius;
+    updateWeather();
+}
+
+// Get user loaction and request for weather data based on location
+function getLocationAndWeather() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+            userLocation = JSON.parse(request.responseText);
+            var weatherUrl = weatherMapApiUrl;
+            weatherUrl = weatherUrl.replace(/{lat}/, userLocation.latitude);
+            weatherUrl = weatherUrl.replace(/{lon}/, userLocation.longitude);
+            getWeatherByCoordinates(weatherUrl);
+        }
+
+    };
+    request.open('GET', userLocationApiUrl, true);
+    request.send()
+}
+
+// Get weather data for location
+function getWeatherByCoordinates(weatherUrl) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+            weatherData = JSON.parse(request.responseText);
+            updateWeather();
+        }
+
+    };
+    request.open('GET', weatherUrl, true);
+    request.send()
+}
+
+function updateWeather() {
+    var currentTemperature;
+    if (isCelsius) {
+        currentTemperature = Number(weatherData.current.temp_c).toFixed(2);
+
+    } else {
+        currentTemperature = Number(weatherData.current.temp_f).toFixed(2);
+    }
+    
+    // Current temperature
+    weatherValueElement = document.getElementById('weather-value')
+    weatherValueElement.innerHTML = String(currentTemperature);
+    
+    // 
+    var weatherUnitData = String.fromCharCode(176) + 'C';
+    document.getElementById('weather-unit').innerHTML = weatherUnitData;
+    
+    var imgDataElement = document.getElementById('weather-image');
+    imgDataElement.setAttribute('src', 'http:' + weatherData.current.condition.icon);
+}
+
